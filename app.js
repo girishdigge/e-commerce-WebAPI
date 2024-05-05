0a66f21c1b35d0274c2d93cac3c54fa6c1f4dd2c
girishdigge <girishdigge@gmail.com>
2024-05-05 13:48:49 +0530

    till env

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//middleware
+app.use(bodyParser.json());
+
+require('dotenv/config');
+const api = process.env.API_URL;
+
+app.get(`${api}/products`, (req, res) => {
+  const products = [{ id: '1', name: 'car', price: '$212,200' }];
+  res.send(products);
+});
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  res.send(newProduct);
+});
+app.listen(3000, () => {
+  console.log('server is running on port 3000...');
+});
